a3583712ed10875fc414a8ef634b0790b7cc11a0
xggggg <gaixia096@gmail.com>
2017-04-25 19:03:56 +0800

    The version which has been used to train two nets for liver segmentation, and test the accuracy and the jaccard coeffcient.

--- unet2d/train.py
+++ unet2d/train.py
@@ -10,7 +10,7 @@ import time
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('train_dir', '//houseware/codalab/Liver_Tumor_Segmentation_Challenge/training_result',
+tf.app.flags.DEFINE_string('train_dir', '//houseware/codalab/Liver_Tumor_Segmentation_Challenge/training_result3',
                            """Directory where to write event logs"""
                            """and checkpoint.""")
 tf.app.flags.DEFINE_integer('max_steps', 10000,
@@ -61,7 +61,7 @@ def train():
             config=tf.ConfigProto(
                 log_device_placement=FLAGS.log_device_placement)) as mon_sess:
             while not mon_sess.should_stop():
-                img, lab = functions.inputs(isTestData=False, useGTData=True, randomly=True)
+                img, lab = functions.inputs(isTestData=False, useGTData=False, randomly=True)
                 mon_sess.run(train_op, feed_dict={x: img, y: lab})
                     
         
